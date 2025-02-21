ba50c8b2f88a4e24c3e7334c17d2a987ba81b7b7
Gabriel Marghoti <gabrielmarghoti@gmail.com>
2025-02-21 01:02:41 +0100

    created

---
+++ RPMotifs.jl
@@ -0,0 +1,18 @@
+module RPMotifs 
+
+using RecurrenceAnalysis
+using DynamicalSystems
+using DifferentialEquations
+using Plots
+using StatsPlots
+using Measures
+using Statistics
+using ProgressBars
+using LinearAlgebra
+using LaTeXStrings
+using DelayEmbeddings
+using InformationMeasures
+using Distances 
+using StatsBase  
+using Dates
+
